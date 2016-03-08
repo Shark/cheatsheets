c7fd4a4558e1e0320b13657bbc716d992196804a
Felix Seidel <felix@seidel.me>
2016-03-08 21:48:06 +0100

    Add Vim cheat sheet

---
+++ source/vim.rb
@@ -0,0 +1,48 @@
+cheatsheet do
+  title 'Vim (Shark)'
+  docset_file_name 'vim-shark'
+  keyword 'vimshark'
+
+  introduction 'Vim commands that I find useful'
+
+  category do
+    id 'vimdiff'
+
+    entry do
+      command 'do'
+      name 'Diff obtain'
+      notes 'Apply changes from the other buffer at the cursor position'
+    end
+
+    entry do
+      command 'dp'
+      name 'Diff put'
+      notes 'Get changes from the other buffer at the cursor position'
+    end
+
+    entry do
+      command 'zo'
+      name 'Open folded text'
+    end
+
+    entry do
+      command 'zc'
+      name 'Close folded text'
+    end
+
+    entry do
+      command ':diffupdate'
+      name 'Re-scan both files for differences'
+    end
+
+    entry do
+      command ']c :'
+      name 'Next difference'
+    end
+
+    entry do
+      command '[c :'
+      name 'Previous difference'
+    end
+  end
+end
